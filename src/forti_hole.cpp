fe902d8570a54bdb0d2335a7d700875d8439e236
Cooper Larson <cooper.larson1@gmail.com>
2024-08-30 16:44:16 -0600

    refactor core method

--- src/forti_hole.cpp
+++ src/forti_hole.cpp
@@ -5,33 +5,43 @@
 #include "include/forti_hole.h"
 #include <forti_api.hpp>
 #include <thread>
+#include <filesystem>
+#include <fstream>
 
 FortiHole::FortiHole() : scraper(), config(scraper.config) {}
 
 void FortiHole::update_threat_feeds() {
-    auto lists = scraper();
+    scraper();
     auto category = config.categories.base;
 
-    for (unsigned int security_level = 0; security_level < lists.size(); ++security_level) {
-        auto file_name_prefix = std::format("forti-hole_security-level-{}_part-", std::to_string(security_level));
+    for (const auto& entry : std::filesystem::directory_iterator("output")) {
+        if (entry.is_regular_file()) {
+            std::string filename = entry.path().string();
 
-        unsigned int num_files = lists[security_level].size();
-        for (unsigned int file_index = 0; file_index < num_files; ++file_index) {
-            auto filename = file_name_prefix + std::to_string(file_index + 1);
+            std::ifstream infile(filename);
+            if (!infile) {
+                std::cerr << "Failed to open file: " << filename << std::endl;
+                continue;
+            }
+
+            std::vector<std::string> lines;
+            std::string line;
+            while (std::getline(infile, line)) lines.push_back(line);
+
+            infile.close();
 
             if (!ThreatFeed::contains(filename)) ThreatFeed::add(filename, category);
-            ThreatFeed::update_feed({filename, lists[security_level][file_index]});
+            ThreatFeed::update_feed({filename, lines});
 
-            // sleep for 3 seconds to give the FortiGate a chance to process the new data,
-            // reduces likelihood of causing network interruptions from buffer overflow.
-            if (file_index < num_files - 1) std::this_thread::sleep_for(std::chrono::seconds(3));
+            // give the FortiGate a chance to process the new data,
+            // prevents network interruptions from buffer overflow
+            std::this_thread::sleep_for(std::chrono::seconds(3));
             ++category;
         }
-
-        remove_extra_files(file_name_prefix, num_files + 1);
     }
 }
 
+// TODO: Reintegrate this in a more robust manner
 void FortiHole::remove_extra_files(const std::string &naming_prefix, unsigned int index) {
     while (true) {
         auto filename = naming_prefix + std::to_string(index);

95ce3e8b98ab9b4ac09491efe475663217903a54
Cooper Larson <cooper.larson1@gmail.com>
2024-08-30 16:37:20 -0600

    implement main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include "include/forti_hole.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    auto start = std::chrono::high_resolution_clock::now();
+    std::cout << "Starting forti-hole..." << std::endl;
+
+    FortiHole fortiHole;
+    fortiHole.update_threat_feeds();
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    std::cout << "forti-hole finished in " << duration.count() << "ms" << std::endl;
+
     return 0;
 }
